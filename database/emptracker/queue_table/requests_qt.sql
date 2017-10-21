d2038ac7a9e4b06757049d46143a1043f3500f1d
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2017-10-21 12:36:28 +0200

    enable message grouping for request queue table

--- database/emptracker/queue_table/requests_qt.sql
+++ database/emptracker/queue_table/requests_qt.sql
@@ -3,7 +3,8 @@ BEGIN
       queue_table          => 'REQUESTS_QT',
       queue_payload_type   => 'SYS.AQ$_JMS_TEXT_MESSAGE',
       sort_list            => 'PRIORITY,ENQ_TIME',
-      multiple_consumers   => TRUE
+      multiple_consumers   => TRUE,
+      message_grouping     => dbms_aqadm.transactional
    );
 END;
 /
