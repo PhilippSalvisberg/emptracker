b8172790904904dd5eb5c8f8be6ddecbc209fe67
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2017-10-21 12:35:41 +0200

    add subscription and PL/SQL callback procedure

--- database/emptracker/queue/requests_aq.sql
+++ database/emptracker/queue/requests_aq.sql
@@ -19,3 +19,29 @@ BEGIN
    );
 END;
 /
+
+-- subscribe to all raw messages (not aggregated messages)
+BEGIN
+   dbms_aqadm.add_subscriber(
+      queue_name => 'requests_aq',
+      subscriber => sys.aq$_agent('RAW_ENQ', 'REQUESTS_AQ', 0),
+      rule       => q'[coalesce(tab.user_data.get_string_property('msg_type'), 'RAW') = 'RAW']'
+   );
+END;
+/
+
+-- register PL/SQL callback procedure to aggregate messages
+BEGIN
+   dbms_aq.register(
+      reg_list => sys.aq$_reg_info_list(
+                     sys.aq$_reg_info(
+                        name      => 'REQUESTS_AQ:RAW_ENQ',
+                        namespace => DBMS_AQ.NAMESPACE_AQ,
+                        callback  => 'plsql://raw_enq_callback?PR=0',
+                        context   => NULL
+                     )
+                  ),
+      reg_count => 1
+   );
+END;
+/
