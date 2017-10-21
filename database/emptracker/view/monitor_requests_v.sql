4bcfa2d8868680a6e4f9db5b1be8dd711c58e5ea
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2017-10-21 12:23:28 +0200

    add transaction identifier, step number and message type columns

--- database/emptracker/view/monitor_requests_v.sql
+++ database/emptracker/view/monitor_requests_v.sql
@@ -1,6 +1,8 @@
 CREATE OR REPLACE VIEW monitor_requests_v AS
 SELECT q.msgid AS msg_id,
        q.corrid AS corr_id,
+       q.enq_tid,
+       q.step_no,
        decode(q.state,
               0,
               'READY',
@@ -17,13 +19,14 @@ SELECT q.msgid AS msg_id,
               'UNKNOWN') AS msg_state,
        h.retry_count AS retry_count,
        coalesce(s.name, h.name) AS consumer_name,
+       coalesce(q.user_data.get_string_property('msg_type'), 'RAW') AS msg_type,
        q.user_data.get_string_property('ename') AS ename,
        q.user_data.get_double_property('old_sal') AS old_sal,
        q.user_data.get_double_property('new_sal') AS new_sal,
        q.user_data.text_vc AS msg_text,
        q.enq_time AS enq_timestamp,
        q.deq_time AS deq_timestamp, -- updated asynchronously, 30 seconds later than "real" dequeue time is not unusual
-       q.deq_time - q.enq_time AS time_in_system -- time spent in the system until final status of message has be "registered" 
+       q.deq_time - q.enq_time AS time_in_system -- time spent in the system until final status of message has be "registered"
   FROM requests_qt q
   JOIN aq$_requests_qt_h h
     ON h.msgid = q.msgid
