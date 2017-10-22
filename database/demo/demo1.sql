cd1bc84fae51d16fe5c51e95cfb4697b0d242446
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2017-10-22 11:38:31 +0200

    add installation scripts and montitor queries

--- database/demo/demo1.sql
+++ database/demo/demo1.sql
@@ -1,3 +1,7 @@
+-- reinstall
+@@../uninstall.sql
+@@../install.sql
+
 -- 5 Tweets
 UPDATE emp SET sal = sal * 2;
 ROLLBACK;
@@ -5,9 +9,18 @@ UPDATE emp SET sal = sal + 100 WHERE JOB = 'SALESMAN';
 UPDATE emp SET sal = sal + 200 WHERE ename IN ('MARTIN' ,'SCOTT');
 COMMIT;
 
+-- Monitor queues
+SELECT * FROM MONITOR_REQUESTS_V;
+SELECT * FROM MONITOR_RESPONSES_V;
+SELECT * FROM MONITOR_REQ_RES_V;
+
 -- 1 Tweet
 UPDATE emp SET sal = sal - 10 WHERE ename = 'SCOTT';
 UPDATE emp SET sal = sal - 20 WHERE ename = 'SCOTT';
 UPDATE emp SET sal = sal - 30 WHERE ename = 'SCOTT';
 UPDATE emp SET sal = sal - 40 WHERE ename = 'SCOTT';
+COMMIT;
+
+-- 1 Tweet
+UPDATE emp SET sal = sal + 100 WHERE ename = 'SCOTT';
 COMMIT;
