062b90bec32c9431e90ce2745bfe7ece625a6909
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2017-10-29 22:38:12 +0100

    add Spring Boot application class and remove old Main class

---
+++ src/main/java/com/salvis/emptracker/EmptrackerApplication.java
@@ -0,0 +1,11 @@
+package com.salvis.emptracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmptrackerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EmptrackerApplication.class, args);
+	}
+}
