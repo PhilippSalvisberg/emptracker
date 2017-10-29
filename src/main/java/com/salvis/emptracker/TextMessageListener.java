ae9dc63a93e27995fabb72727c002da49fcd1bbc
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2017-10-29 22:42:31 +0100

    use Spring Boot configuration, extend error handling and support tweet text from JMS message body

--- src/main/java/com/salvis/emptracker/TextMessageListener.java
+++ src/main/java/com/salvis/emptracker/TextMessageListener.java
@@ -12,40 +12,40 @@ import javax.jms.TopicSession;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.listener.SessionAwareMessageListener;
 import org.springframework.stereotype.Component;
 
 import oracle.jms.AQjmsAgent;
 import oracle.jms.AQjmsTopicPublisher;
+import twitter4j.Status;
+import twitter4j.Twitter;
 
 @Component
 public class TextMessageListener implements SessionAwareMessageListener<TextMessage> {
 	private final Logger logger = Logger.getLogger(TextMessageListener.class.getName());
 
 	@Autowired
-	private TwitterEmpTracker twitter;
+	private Twitter twitter;
 
-	@Autowired(required = false)
-	@Qualifier("salaryIncreaseTemplate")
-	private String salaryIncreaseTemplate = "OMG ${ename} got a pay raise of $${more}. Making $${new_sal} a month now. Congrats. #OnCommitTrigger at #DOAG2017";
+	@Value("${msg.salaryIncreaseTemplate}")
+	private String salaryIncreaseTemplate;
 
-	@Autowired(required = false)
-	@Qualifier("salaryDecreaseTemplate")
-	private String salaryDecreaseTemplate = "Oh no, ${ename} got a pay reduction of $${less}. Making $${new_sal} a month now. #OnCommitTrigger at #DOAG2017";
-
+	@Value("${msg.salaryDecreaseTemplate}")
+	private String salaryDecreaseTemplate;
+	
 	private String getText(String ename, Double oldSal, Double newSal) {
 		String template;
 		if (newSal > oldSal) {
 			Double more = newSal - oldSal;
-			template = salaryIncreaseTemplate.replace("${more}", String.format("%.2f", more));
+			template = salaryIncreaseTemplate.replace("@{more}", String.format("%.2f", more));
 		} else {
 			Double less = oldSal - newSal;
-			template = salaryDecreaseTemplate.replace("${less}", String.format("%.2f", less));
+			template = salaryDecreaseTemplate.replace("@{less}", String.format("%.2f", less));
 		}
-		return template.replace("${ename}", ename.substring(0, 1) + ename.substring(1).toLowerCase())
-				.replace("${new_sal}", String.format("%.2f", newSal))
-				.replace("${old_sal}", String.format("%.2f", oldSal));
+		return template.replace("@{ename}", ename.substring(0, 1) + ename.substring(1).toLowerCase())
+				.replace("@{new_sal}", String.format("%.2f", newSal))
+				.replace("@{old_sal}", String.format("%.2f", oldSal));
 	}
 
 	private void sendResponse(final TextMessage request, final Session session, String text) throws JMSException, SQLException {
@@ -91,22 +91,33 @@ public class TextMessageListener implements SessionAwareMessageListener<TextMess
 		try {
 			correlationId = request.getJMSCorrelationID();
 			logger.debug("processing message with correlationId " + correlationId + "...");
-			String ename = request.getStringProperty("ename");
-			Double oldSal = request.getDoubleProperty("old_sal");
-			Double newSal = request.getDoubleProperty("new_sal");
-			String text = getText(ename, oldSal, newSal);
+			String text = request.getText();
+			if (text == null || text.isEmpty()) {
+				String ename = request.getStringProperty("ename");
+				Double oldSal = request.getDoubleProperty("old_sal");
+				Double newSal = request.getDoubleProperty("new_sal");
+				text = getText(ename, oldSal, newSal);
+			}
 			logger.info(text);
-			twitter.updateStatus(text);
-			sendResponse(request, session, text);
+			Status status = twitter.updateStatus(text);
+			String screenName = status.getUser().getScreenName();
+			logger.debug("tweet by " + screenName);
+			sendResponse(request, session, screenName + ": " + text);
 			session.commit();
 		} catch (Exception e) {
 			try {
 				session.rollback(); // increment retry count / expire message
 			} catch (JMSException e1) {
-				logger.error("Cound not rollback session (to increment retry count). Error was : " + e1.getMessage());
+				logger.error("Cound not rollback session (to increment retry count). Error was : " + e1.toString());
 			}
-			String errorText = "message with correlationId " + correlationId + " processed with error: " + e.getMessage();
+			String errorText = "message with correlationId " + correlationId + " processed with error: " + e.toString();
 			logger.error(errorText);
+			try {
+				sendResponse(request, session, errorText);
+				session.commit();
+			} catch (Exception e1) {
+				logger.error("Could not send response for correlationId " + correlationId + ". Got error_ " + e1.toString());
+			}
 			throw new RuntimeException(errorText);
 		}
 	}
