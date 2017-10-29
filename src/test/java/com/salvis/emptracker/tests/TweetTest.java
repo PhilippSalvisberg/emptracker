d5c1eba1f9025c83b80688c4ddf37e25a6cdc9e4
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2017-10-29 22:41:25 +0100

    refactor test using Spring Boot configuration

--- src/test/java/com/salvis/emptracker/tests/TweetTest.java
+++ src/test/java/com/salvis/emptracker/tests/TweetTest.java
@@ -2,16 +2,28 @@ package com.salvis.emptracker.tests;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import com.salvis.emptracker.TwitterEmpTracker;
+import com.salvis.emptracker.AppConfig;
 
 import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Import(AppConfig.class)
 public class TweetTest {
+	
+	@Autowired
+	private Twitter twitter;
 
 	@Test
-	public void postTweet() {
-		TwitterEmpTracker twitter = new TwitterEmpTracker();
+	public void postTweet() throws TwitterException {
 		String text = "SCOTT got a pay raise of $150. Making $3000 a month now. Congrats.";
 		Status status = twitter.updateStatus(text);
 		Assert.assertEquals(text, status.getText());
